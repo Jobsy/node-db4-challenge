02b9de7ae6dac26c6e76e8400517031cc2122bcf
Oluwajoba <oluwajobabello@gmail.com>
2019-11-08 07:42:44 +0100

    update up and down export functions / migrations

--- data/migrations/20191108070155_create-tables.js
+++ data/migrations/20191108070155_create-tables.js
@@ -1,8 +1,39 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable("recipe", table => {
+        table.increments()
+        table.string("recipe_name", 128)
+            .notNullable()
+    })
+        .createTable("ingredient", table => {
+            table.increments()
+            table.string("ingredient_name", 128)
+                .notNullable()
+        })
+        .createTable("quantity", table => {
+            table.increments()
+            table.string("quantity", 128)
+            table.integer("recipe_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("recipe")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE")
+            table.integer("ingredient_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("ingredient")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE")
+            table.primary(["recipe_id", "ingredient_id"])
+        })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema
+        .dropTableIfExits("quantity")
+        .dropTableIfExits("ingredient")
+        .dropTableIfExits("recipe")
 };
