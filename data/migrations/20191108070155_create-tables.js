9f1c4f66723005b86c60ba3983ac728d46a2d488
Oluwajoba <oluwajobabello@gmail.com>
2019-11-11 02:19:44 +0100

    added description table / data/migration

--- data/migrations/20191108070155_create-tables.js
+++ data/migrations/20191108070155_create-tables.js
@@ -1,14 +1,27 @@
 
 exports.up = function (knex) {
-    return knex.schema.createTable("recipe", table => {
-        table.increments()
-        table.string("recipe_name", 128)
-            .notNullable()
-    })
+    return knex.schema
+        .createTable("recipe", table => {
+            table.increments()
+            table.string("recipe_name", 128)
+                .notNullable()
+        })
         .createTable("ingredient", table => {
             table.increments()
             table.string("ingredient_name", 128)
                 .notNullable()
+        })
+        .createTable("description", table => {
+            table.increments()
+            table.string("recipe_description", 128)
+                .notNullable()
+            table.integer("recipe_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("recipe")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE")
         })
         .createTable("quantity", table => {
             table.increments()
