3ab83fe7f113710cb8284060c96e5b2943840902
Oluwajoba <oluwajobabello@gmail.com>
2019-11-11 05:48:40 +0100

    update db.js getInstruction() / add router get(/:id/instructions) / recipes-router.js

--- data/recipes-router.js
+++ data/recipes-router.js
@@ -19,7 +19,7 @@ router.get("/", (req, res) => {
         })
 })
 
-router.get("/:id", (req, res) => {
+router.get("/:id/shoppingList", (req, res) => {
     const { id } = req.params;
     const { url } = req;
 
@@ -36,20 +36,20 @@ router.get("/:id", (req, res) => {
         })
 })
 
-router.get("/:id", (req, res) => {
+router.get("/:id/instructions", (req, res) => {
     const { id } = req.params;
     const { url } = req;
 
     dB.getInstructions(id)
-        .then((shoppingList) => {
+        .then((getInstructions) => {
 
-            if (shoppingList.length === 0) {
-                res.status(404).json({ message: "The shoppingList with the specified ID does not exist." })
+            if (getInstructions.length === 0) {
+                res.status(404).json({ message: "The getInstructions with the specified ID does not exist." })
             }
-            res.status(200).json({ shoppingListInfo: shoppingList, url: url, operation: "GET" })
+            res.status(200).json({ getInstructionsInfo: getInstructions, url: url, operation: "GET" })
         })
         .catch((err) => {
-            res.status(500).json({ error: "The shoppingList information could not be retrieved." + err })
+            res.status(500).json({ error: "The getInstructions information could not be retrieved." + err })
         })
 })
 
