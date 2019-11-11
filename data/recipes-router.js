05885d1d9b311ffd3169ad22d6a81ead01ac7582
Oluwajoba <oluwajobabello@gmail.com>
2019-11-11 04:56:50 +0100

    create and update recipes-router.js

---
+++ data/recipes-router.js
@@ -0,0 +1,10 @@
+
+const express = require("express");
+
+const dB = require("./db");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.status(200).json("It's workinggggggggggggggggg!!!")
+})
