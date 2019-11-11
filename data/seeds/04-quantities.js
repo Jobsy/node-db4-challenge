b80a75dfee0ebf4d25ce266d3a31e9748ccea8da
Oluwajoba <oluwajobabello@gmail.com>
2019-11-11 04:30:15 +0100

    create and update 04-quantities.js / seeds

---
+++ data/seeds/04-quantities.js
@@ -0,0 +1,7 @@
+exports.seed = function(knex) {
+  return knex("quantities").insert([
+        {recipe_id: 3, ingredient_id: 2, quantity: "5kg"},
+        {recipe_id: 2, ingredient_id: 3, quantity: "2g"},
+        {recipe_id: 1, ingredient_id: 1, quantity: "3.5g"},
+      ]);
+};
